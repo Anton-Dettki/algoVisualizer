e5fffa61f22459683c5249e06610eee1c5855b89
Anton Dettki <caspardett@gmail.com>
2025-05-21 21:34:02 +0200

    First dabbling

--- algo-visualizer/src/app/page.js
+++ algo-visualizer/src/app/page.js
@@ -1,95 +1,82 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
+import { use, useEffect, useState } from 'react'
+
+function Matrix() {
+    const [rows, setRows] = useState(20);
+    const [cols, setCols] = useState(25);
+    const [adjList, setList] = useState([])
+    const [grid, setGrid] = useState([])
+
+    const changeRows = (event, newValue) => { setRows(newValue) }
+    const changeCols = (event, newValue) => { setCols(newValue) }
+
+    useEffect(() => {
+        computeGrid()
+    }, [])
+
+    function computeGrid() {
+        const matr = Array.from({ length: rows }, () =>
+            Array.from({ length: cols }, () => 0)
+        );
+        computeAdjacencyList()
+        setGrid(matr)
+        console.log("GRID ", grid)
+    }
+
+    function computeAdjacencyList (rows, cols) {
+        const adjList = new Map();
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                const nodeId = r * cols + c;
+                const neighbors = [];
+
+                if (r > 0) neighbors.push((r - 1) * cols + c); // up
+                if (r < rows - 1) neighbors.push((r + 1) * cols + c); // down
+                if (c > 0) neighbors.push(r * cols + (c - 1)); // left
+                if (c < cols - 1) neighbors.push(r * cols + (c + 1)); // right
+
+                adjList.set(nodeId, neighbors)
+            }
+        }
+
+        console.log("ADJ", adjList)
+        setList(adjList)
+    }
+
+    return (
+        <>
+            <div className="flex-box">
+                <Slider value={rows} onChange={changeRows}></Slider>
+                Rows: { rows }
+                <Slider value={cols} onChange={changeCols}></Slider>
+                Cols: size { cols }
+                <Button onClick={computeGrid}> Compute</Button>
+            </div>
+
+            <div className="inline-block">
+                {grid.map((row, rowIndex) => (
+                    <div key={rowIndex} className="flex">
+                        {row.map((value, colIndex) => (
+                            <Button key={colIndex}> {value} </Button>
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+        </>
+    )
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+return (
+    <>
+      <Matrix />
+    </>
+)
 }
+
+
